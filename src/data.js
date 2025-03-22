836020e09b5de4be5688bdceaff8342664beca79
mhdrdn122 <mhdrdn122@gmail.com>
2025-03-22 12:15:13 +0300

    add new project

--- src/data.js
+++ src/data.js
@@ -1,62 +1,103 @@
 export const data = [
-    {id:1 ,img:"images/selecac.PNG", title:"selecao" ,
-    disc:"One page website using html and bootstrap and animate.css ",
-    demo:"https://mhdrdn122.github.io/selecao/",
+  {
+    id: 1,
+    img: "images/selecac.PNG",
+    title: "selecao",
+    disc: "One page website using html and bootstrap and animate.css ",
+    demo: "https://mhdrdn122.github.io/selecao/",
     code: "https://github.com/mhdrdn122/selecao",
-    category:"bootstrap" },
+    category: "bootstrap",
+  },
 
-    {id:2 ,img:"images/laura.PNG", title:"laura" ,
-    disc:"One page website using pure html and css ",
-    demo:"https://mhdrdn122.github.io/laura/main.html?fbclid=IwAR22kT1a28IDX8vLZkCXFpdJjLSYECMesqKpyJj18p7NN7f085EcsqPgU74",
+  {
+    id: 2,
+    img: "images/laura.PNG",
+    title: "laura",
+    disc: "One page website using pure html and css ",
+    demo: "https://mhdrdn122.github.io/laura/main.html?fbclid=IwAR22kT1a28IDX8vLZkCXFpdJjLSYECMesqKpyJj18p7NN7f085EcsqPgU74",
     code: "https://github.com/mhdrdn122/laura",
-    category:"pure css" },
+    category: "pure css",
+  },
 
-    {id:3 ,img:"images/cruds.jpeg", title:"cruds" ,
-    disc:"A simple programing project using HTML, CSS and JavaScript This project contains the following basic operations { Create , Read , Updata , Delete , Search }",
-    demo:"https://mhdrdn122.github.io/Cruds/",
+  {
+    id: 3,
+    img: "images/cruds.jpeg",
+    title: "cruds",
+    disc: "A simple programing project using HTML, CSS and JavaScript This project contains the following basic operations { Create , Read , Updata , Delete , Search }",
+    demo: "https://mhdrdn122.github.io/Cruds/",
     code: "https://github.com/mhdrdn122/Cruds",
-    category:"javaScript" },
+    category: "javaScript",
+  },
 
-    {id:4 ,img:"images/dashboard.jpeg", title:"dashboard" ,
-    disc:"A professional dashboard consisting of 8 pages and responsive to all screens. In this project, a special framework was built",
-    demo:"https://mhdrdn122.github.io/dashsboard/",
+  {
+    id: 4,
+    img: "images/dashboard.jpeg",
+    title: "dashboard",
+    disc: "A professional dashboard consisting of 8 pages and responsive to all screens. In this project, a special framework was built",
+    demo: "https://mhdrdn122.github.io/dashsboard/",
     code: "https://github.com/mhdrdn122/dashsboard",
-    category:"pure css" },
+    category: "pure css",
+  },
 
-    {id:5 ,img:"images/pro3.jpeg", title:"project" ,
-    disc:"A project from one page has animation and hover and responsive using html and css and Icons",
-    demo:"https://mhdrdn122.github.io/Project-3/",
+  {
+    id: 5,
+    img: "images/pro3.jpeg",
+    title: "project",
+    disc: "A project from one page has animation and hover and responsive using html and css and Icons",
+    demo: "https://mhdrdn122.github.io/Project-3/",
     code: "https://github.com/mhdrdn122/Project-3",
-    category:"pure css" },
+    category: "pure css",
+  },
 
-    // {id:6 ,img:"images/q.PNG", title:"questions" ,
-    //   disc:"A simple project to display questions and answers that uses React js and bootstrap and localstorge",
-    //   demo:"https://mhdrdn122.github.io/question/",
-    //   code: "https://github.com/mhdrdn122/question",
-    //   category:"react" },
+  // {id:6 ,img:"images/q.PNG", title:"questions" ,
+  //   disc:"A simple project to display questions and answers that uses React js and bootstrap and localstorge",
+  //   demo:"https://mhdrdn122.github.io/question/",
+  //   code: "https://github.com/mhdrdn122/question",
+  //   category:"react" },
 
-
-    {id:6 ,img:"images/ecommerce4.jpeg", title:"FOODMART" ,
-    disc:"A modern e-commerce store built with React, featuring a responsive design and a seamless user experience. The project includes a comprehensive admin dashboard for managing products, categories, and users, as well as a shopping cart for easy order management.",
-    demo:"https://ecommerce4-phi.vercel.app/",
+  {
+    id: 6,
+    img: "images/ecommerce4.jpeg",
+    title: "FOODMART",
+    disc: "A modern e-commerce store built with React, featuring a responsive design and a seamless user experience. The project includes a comprehensive admin dashboard for managing products, categories, and users, as well as a shopping cart for easy order management.",
+    demo: "https://ecommerce4-phi.vercel.app/",
     code: "https://github.com/mhdrdn122/ecommerce4",
-    category:"react" },
+    category: "react",
+  },
 
+  // {id:7 ,img:"images/movie.PNG", title:"Movie List " ,
+  // disc:"The movie list project is a project through which you can browse or search for your favorite movie and see various information about this movie when you click on it. In this project, I use react js and axios to fetch data from Api(no deploy)",
+  // demo:"https://mhdrdn122.github.io/moviesWithRedux/",
+  // code: "https://github.com/mhdrdn122/moviesWithRedux",
+  // category:"react" },
 
-    // {id:7 ,img:"images/movie.PNG", title:"Movie List " ,
-    // disc:"The movie list project is a project through which you can browse or search for your favorite movie and see various information about this movie when you click on it. In this project, I use react js and axios to fetch data from Api(no deploy)",
-    // demo:"https://mhdrdn122.github.io/moviesWithRedux/",
-    // code: "https://github.com/mhdrdn122/moviesWithRedux",
-    // category:"react" },
-
-    {id:8 ,img:"images/shop.PNG", title:"ShopCart " ,
-    disc:"Building a complete online store using React js The following techniques were used: React js and firebase and Building the login and Registration Pages on the website and Building the CartPage and Bootstrap",
-    demo:"https://ecommerce2-vert-one.vercel.app/",
+  {
+    id: 8,
+    img: "images/shop.PNG",
+    title: "ShopCart ",
+    disc: "Building a complete online store using React js The following techniques were used: React js and firebase and Building the login and Registration Pages on the website and Building the CartPage and Bootstrap",
+    demo: "https://ecommerce2-vert-one.vercel.app/",
     code: "https://github.com/mhdrdn122/ecommerce2",
-    category:"react" }
+    category: "react",
+  },
 
+  {
+    id: 9,
+    img: "images/matdash.png",
+    title: "Dashboard ",
+    disc: " A dashboard project designed using React.js and Material-UI. The dashboard consists of 10 distinct pages and features two elegant and professional color themes. The project highlights a modern, responsive design and offers intuitive navigation to enhance user experience."
+    ,
+    demo: "https://dashboard-react-ten.vercel.app/",
+    code: "https://github.com/mhdrdn122/dashsboard",
+    category: "react",
+  },
+];
 
-  ]
-
-export const categoryBtn = ["all" , "react" ,  "pure css"  , "javaScript" , "bootstrap"  ]
-export const avatar ='images/avatar.jpg'
+export const categoryBtn = [
+  "all",
+  "react",
+  "pure css",
+  "javaScript",
+  "bootstrap",
+];
+export const avatar = "images/avatar.jpg";
